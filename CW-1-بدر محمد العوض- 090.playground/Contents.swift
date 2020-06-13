30e6b20cc78ac894bae6d19b5eaf506afcdb9d2e
Baderkw <66323674+Baderkw@users.noreply.github.com>
2020-06-13 19:02:16 +0300

    تم الانتهاء من حل التمرين

---
+++ CW-1-بدر محمد العوض- 090.playground/Contents.swift
@@ -0,0 +1,23 @@
+let name = "بدر محمد العوض"
+
+var height = 1.74
+var age = 17
+var hobby1 = "حل مكعب روبك"
+var hobby2 = "البرمجة"
+
+print("السلام عليكم، اسمي \(name) انا احب \(hobby1) و \(hobby2) عمري \(age) و طولي \(height)")
+
+
+
+let friendName = "علي العوض"
+var friendHeight = 1.76
+var friendAge = 17
+var friendHobby1 = "لعب العاب الفيديو"
+var friendHobby2 = "السباحة"
+
+var age_difference = age - friendAge
+
+height != friendHeight
+age == friendAge
+height >= friendHeight
+
